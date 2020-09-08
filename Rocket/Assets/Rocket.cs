422262ea09bc1c5c785c1f55253001a7d555dddd
Frederic_Guay <fredericguay@hotmail.ca>
2020-09-08 00:03:58 -0400

    Script mouvement fait mais non fonctionnel

---
+++ Rocket/Assets/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    RigidBody myRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = gameObject.GetComponent<RigidBody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputHeight = Input.GetAxis("jump");
+        myRigidBody.AddForce(new Vector3(0, inputHeight, 0);
+    }
+}
